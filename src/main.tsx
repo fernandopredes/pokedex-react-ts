8c3cc804097f87c32df7537c665cfd9de2dedb24
fernando predes <fernando_predes@hotmail.com>
2022-11-05 22:41:17 -0300

    ações do slice

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider} from "react-router-dom";
 import router from './router';
+import {Provider} from 'react-redux'
+import {store} from './redux/index'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <section className='container'>
-    <RouterProvider  router={router} />
+    <Provider store={store}>
+      <section className='container'>
+      <RouterProvider  router={router} />
 
-    </section>
+      </section>
+    </Provider>
   </React.StrictMode>
 )
